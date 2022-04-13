f0bd6ffd5dd1c5bf2a1bbffe843e04f9e73fb5ec
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-04-13 21:15:36 +0100

    add rareusdc

--- scripts/validatePools.js
+++ scripts/validatePools.js
@@ -127,7 +127,7 @@ const validatePools = async () => {
       uniqueOracleId.add(pool.oracleId);
 
       const { keeper, strategyOwner, vaultOwner, beefyFeeRecipient } =
-        addressBook[chain].platforms.magikfarmssss;
+        addressBook[chain].platforms.magikfarms;
 
       updates = isKeeperCorrect(pool, chain, keeper, updates);
       updates = isStratOwnerCorrect(pool, chain, strategyOwner, updates);
