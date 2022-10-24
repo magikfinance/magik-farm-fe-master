69e302defbb48462e380cf7ef4ed4d7392452fe9
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-10-25 01:28:58 +0300

    fix

--- src/features/configure/vault/fantom_pools.js
+++ src/features/configure/vault/fantom_pools.js
@@ -9,7 +9,7 @@ export const fantomPools = [
     tokenDecimals: 18,
     tokenDescriptionUrl: '#',
     earnedToken: 'magikfarmMAGIK',
-    earnedTokenAddress: 'c',
+    earnedTokenAddress: '0x0365b8Aa14D5Eecea962C28e4531D1bc2a22D612',
     earnContractAddress: '0x0365b8Aa14D5Eecea962C28e4531D1bc2a22D612',
     pricePerFullShare: 1,
     withdrawalFee: '0.5%',
